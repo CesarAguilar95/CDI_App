2e835810807c793adbb1d5fb1d097843711627ab
CesarAguilar95 <111412458+CesarAguilar95@users.noreply.github.com>
2022-09-11 18:58:09 -0500

    Se sube el proyecto para pruebas

---
+++ CDI_App/Main.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace CDI_App
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.UsuarioTexto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ContraseñaTexto = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UsuarioTexto
+            // 
+            this.UsuarioTexto.Location = new System.Drawing.Point(53, 79);
+            this.UsuarioTexto.Name = "UsuarioTexto";
+            this.UsuarioTexto.Size = new System.Drawing.Size(169, 20);
+            this.UsuarioTexto.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("RomanD", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("RomanD", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Contraseña";
+            // 
+            // ContraseñaTexto
+            // 
+            this.ContraseñaTexto.Location = new System.Drawing.Point(54, 134);
+            this.ContraseñaTexto.Name = "ContraseñaTexto";
+            this.ContraseñaTexto.PasswordChar = '*';
+            this.ContraseñaTexto.Size = new System.Drawing.Size(169, 20);
+            this.ContraseñaTexto.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(229, 135);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(49, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Ver";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 306);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ContraseñaTexto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UsuarioTexto);
+            this.Controls.Add(this.button1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox UsuarioTexto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ContraseñaTexto;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ CDI_App/Main.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CDI_App
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            //float flotante;
+            //int entero;
+            //bool booleana;
+            //double doble;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string User, Pass;
+
+            User = "Admin";
+            Pass = "1234";
+
+            if (UsuarioTexto.Text == "") //comparacion de usuario que no es igual a ""
+            {
+                MessageBox.Show("No se ha insertado un usuario");
+            }
+            else 
+            {
+                if(ContraseñaTexto.Text == "") //comparacion de contraseña no esta vacia
+                {
+                    MessageBox.Show("No se ha insertado una contraseña");
+                }
+                else
+                {
+                    if(User == UsuarioTexto.Text)//comparacion de si existe el usuario
+                    {
+                        if(Pass == ContraseñaTexto.Text)//comparacion de contraseña
+
+                        {
+                            MessageBox.Show("Bienvenido al sistema CDI");
+                        }
+                        else //aqui nos dice si esta mal la contraseña
+                        {
+                            ContraseñaTexto.Text = "";
+                            MessageBox.Show("Contraseña incorrecta");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("El usuario no existe");
+                    }
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+         if(checkBox1.Checked == true)
+            {
+                ContraseñaTexto.PasswordChar = '\0';
+            }
+            else
+            {
+                ContraseñaTexto.PasswordChar = '*';
+            }
+        }
+    }
+}
